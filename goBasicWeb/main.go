d2523a9c0c8fd31334c0bbdae34fd088db4667f0
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-18 15:33:27 -0500

    goBasicWeb | fix: resolve incorrect FormValue name

--- goBasicWeb/main.go
+++ goBasicWeb/main.go
@@ -6,8 +6,20 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "name = %s\n", name)
+	fmt.Fprintf(w, "email = %s\n", email)
+}
+
 func greetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "hello" {
+	if r.URL.Path != "/greet" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
@@ -24,10 +36,10 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
-	http.HandlerFunc("/greet", greetHandler)
+	http.HandleFunc("/greet", greetHandler)
 
-	fmt.Println("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil (
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
-	)
+	}
 }
