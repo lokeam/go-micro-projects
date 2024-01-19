d03b9a01f85f96c170fbdc011281ea68ccd69b15
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-18 22:07:35 -0500

    goEmailVerify | build: project + scope setup

---
+++ goEmailVerify/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	scanner := bufio.NewScanner()
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, dmarcRecord\n")
+
+	// Check the syntax of the email
+
+	// Verify the domain of the email address
+
+	// Check the mail exchange records
+
+	// Ping the email server
+
+	// Detect disposable email address
+}
+
+func checkDomain(domain string) {
+
+}
