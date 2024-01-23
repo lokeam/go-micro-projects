84897d8b4b5a73e6cd994ddb91540369cf684b2b
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-22 22:57:22 -0500

    goWebHtmx | feat: display server data in templates

---
+++ goWebHtmx/go.mod
@@ -0,0 +1,3 @@
+module sixam/goWebHtmx
+
+go 1.21.6
--- goWebHtmx/main.go
+++ goWebHtmx/main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type Album struct {
+	Title  string
+	Artist string
+}
+
 func main() {
 	fmt.Println("Hello world")
 
-	handlerOne := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello World")
-		io.WriteString(w, r.Method)
+	templateHandler := func(w http.ResponseWriter, r *http.Request) {
+		webtemplate := template.Must(template.ParseFiles("index.html"))
 
+		/* Sample Album data */
+		albums := map[string][]Album{
+			"Albums": {
+				{Title: "Led Zeppelin", Artist: "Led Zeppelin IV"},
+				{Title: "Rolling Stones", Artist: "Tattoo You"},
+				{Title: "David Bowie", Artist: "Diamond Dogs"},
+				{Title: "The Meters", Artist: "Look-Ky Py Py"},
+				{Title: "Journey", Artist: "Escape"},
+				{Title: "Otis Redding", Artist: "Otis Blue"},
+				{Title: "The Black Crowes", Artist: "The Southern Harmony and Musical Companion"},
+			},
+		}
+
+		webtemplate.Execute(w, albums)
 	}
-	http.HandleFunc("/", handlerOne)
+	http.HandleFunc("/", templateHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
-
 }
